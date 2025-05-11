4c1a366a702c5f58d0e1e9f3553711d3886c4cda
ErvinGU <ervin.garay+1@gmail.com>
2025-05-11 23:13:58 +0000

    actividad 1

--- escape.py
+++ escape.py
@@ -1,11 +1,13 @@
-
+import math as m
 
 
 #Calcular velocidad de escape
 #formula : √ 2*G*r
 
-g = imput("ingrese constante de gravedad")
-r = imput("ingrese el radio en kilometros")
+g = float(input("ingrese constante de g [m/s²] : "))
+r = float(input("ingrese el radio en kilometros [Km] : "))
 
+ve = (m.sqrt(2 * g * (r * 1000)))
 
+print(f"La velocidad de Escape es : {ve:.1f} [m/s]")
 
